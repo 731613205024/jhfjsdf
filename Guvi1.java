bea8c6b2b670cd79d8efc59fc7da94730d796f57
731613205024 <731613205024@users.noreply.github.com>
2016-09-28 08:58:42 +0530

    Add files via upload

---
+++ Guvi1.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guvi1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Guvi1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+
+
+        public static String main(String[] args) {
+        
+    
+        Scanner sc=new Scanner(System.in);
+        String str;
+        System.out.println("ENETR YOUR STRING=\n");
+        
+        str=sc.nextLine();
+     if (str.length() <= 1) 
+     {
+         return str;
+     }
+  String mid = str.substring(1, str.length()-1);
+  //System.out.println(" YOUR STRING= " +String mid);
+ 
+  return str.charAt(str.length()-1) + mid + str.charAt(0);
+       
+}
+    }
+    
+
